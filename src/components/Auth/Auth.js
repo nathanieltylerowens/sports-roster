83f0cda0958f973ae8567e9cbee1fa86e72bdb42
NateOwens <nathanieltylerowens@gmail.com>
2020-08-25 17:54:21 -0500

    added auth

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleProvider);
+  }
+
+  render() {
+    return (
+      <div className="Auth">
+        <button className="btn btn-dark" onClick={this.loginClickEvent}>Google Log In</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ src/components/Team/Team.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Team extends React.Component {
+  render() {
+    return (
+      <h2>Team here</h2>
+    );
+  }
+}
+
+export default Team;
